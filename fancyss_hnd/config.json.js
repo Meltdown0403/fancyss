ec57360222c7da635defbc43cfd53523dd4a0d0b
Meltdown0403 <junjun0403@163.com>
2021-01-23 02:14:48 +0900

    update fancyss_hnd 1.9.3

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -4,7 +4,7 @@
 "home_url":"Module_shadowsocks.asp",
 "md5":"7f2071c1cf8d8947e9ad88daab920993",
 "name":"shadowsocks",
-"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
+"tar_url": "https://raw.githubusercontent.com/Meltdown0403/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
 "version":"1.9.3"
 }
